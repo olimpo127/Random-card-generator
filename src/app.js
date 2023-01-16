60ed804ca1a5034c166b766401d3aaec95401114
F.Ojeda.I <fojedai47@gmail.com>
2023-01-16 23:43:44 +0000

    third commit

--- src/app.js
+++ src/app.js
@@ -9,3 +9,13 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+let numbers = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+let kinds = ["Hearts", "Clovers", "Diamonds", "Spades"];
+
+let randomNumber = numbers[Math.floor(Math.random() * numbers.length)];
+let randomKind = kinds[Math.floor(Math.random() * kinds.length)];
+
+let finalCard = document.querySelector("#number");
+
+finalCard.innerHTML = randomNumber;
